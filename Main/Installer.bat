1669ae322e2343e94d3b02334c5e7a693c13d3f2
CQHofsns <45648109+CQHofsns@users.noreply.github.com>
2019-02-01 13:02:14 +0900

    updata installer.bat

--- Main/Installer.bat
+++ Main/Installer.bat
@@ -1,4 +1,40 @@
 @echo off
+
+echo Checking Python version:
+python --version
+if errorlevel 1 echo Python is not found.
+
+echo(
+echo Install required packages:
+
+echo(
+echo Installing "Pillow" package:
+python -m pip install Pillow
+if errorlevel 1 (
+	echo Failed to install numpy package.
+	pause)else echo Install successfully !
+
+echo(
+echo Installing "ReGex" package:
+python -m pip install regex
+if errorlevel 1 (
+	echo Failed to install numpy package.
+	pause)else echo Install successfully !
+
+echo(
+echo Installing "Numpy" package:
+python -m pip install numpy
+if errorlevel 1 (
+	echo Failed to install numpy package.
+	pause)else echo Install successfully !
+
+echo(
+echo Installing "Opencv-python" package:
+python -m pip install opencv-python
+if errorlevel 1 (
+	echo Failed to install numpy package.
+	pause)else echo Install successfully !
+
 cd ..
 cd "%cd%\Font"
 
@@ -9,9 +45,10 @@ start NotoSansCJKsc-Black.otf
 start NotoSansCJKtc-Black.otf
 
 cd ..
-cd "%cd%\source"
+cd "%cd%\Main"
 
-@echo python %cd%\source.py > Start-Program.bat
-@echo pause >> Start-Program.bat
+@echo python %cd%\source.py > Run.bat
+@echo echo %cd% >> Run.bat
+@echo pause >> Run.bat
 
 exit
